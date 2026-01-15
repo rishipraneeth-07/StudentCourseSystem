7125201e7e534b8b82a24c3e3651554722f34082
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-15 22:01:07 +0530

    Validated student entity

--- src/main/java/com/todo/studentcoursesystem/Entity/Student.java
+++ src/main/java/com/todo/studentcoursesystem/Entity/Student.java
@@ -1,20 +1,37 @@
 package com.todo.studentcoursesystem.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "student")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(length = 50, nullable = false,name = "full_name")
     private String fullName;
+
+
+    @Column(unique = true, nullable = false)
     private String email;
-    private long rollNumber;
+
+
+    @Column(unique = true, nullable = false,name = "roll_number")
+    private String rollNumber;
+
+    @Column(nullable = false,name = "created_on")
     private LocalDateTime createdOn;
 
 
