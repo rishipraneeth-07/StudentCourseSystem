582a8cc08eb544a2811160323722e95236f6b32c
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-15 21:21:58 +0530

    Initial commit

---
+++ src/main/java/com/todo/studentcoursesystem/StudentCourseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.todo.studentcoursesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCourseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todo/studentcoursesystem/StudentCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.studentcoursesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
