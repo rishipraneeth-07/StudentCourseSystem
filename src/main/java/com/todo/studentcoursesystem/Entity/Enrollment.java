e915e64cb382bac754f59f7625ea15fa6b3a272c
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-16 19:07:55 +0530

    Added Enrollment entity

---
+++ src/main/java/com/todo/studentcoursesystem/Entity/Enrollment.java
@@ -0,0 +1,14 @@
+package com.todo.studentcoursesystem.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
