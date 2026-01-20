49b4755da7fbc5e452f3b15a101cadd8d6ea6246
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-20 23:42:19 +0530

    Course Controller with methods

--- src/main/java/com/todo/studentcoursesystem/Controller/CourseController.java
+++ src/main/java/com/todo/studentcoursesystem/Controller/CourseController.java
@@ -1,4 +1,48 @@
 package com.todo.studentcoursesystem.Controller;
 
+import com.todo.studentcoursesystem.Entity.Course;
+import com.todo.studentcoursesystem.Service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    private CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course createdCourse = courseService.createCourse(course);
+        return ResponseEntity.ok(createdCourse);
+    }
+
+    // 2. Get Course by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable Long id) {
+        Course course = courseService.getCourse(id);
+        return ResponseEntity.ok(course);
+    }
+
+    // 3. Update Course
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> updateCourse(
+            @PathVariable Long id,
+            @RequestBody Course course) {
+
+        Course updatedCourse = courseService.updateCourse(course, id);
+        return ResponseEntity.ok(updatedCourse);
+    }
+
+    // 4. Delete Course
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+        return ResponseEntity.noContent().build();
+    }
 }
