b881e629f2a3a059d549770d9f3f36e3fa6d8128
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-21 20:33:46 +0530

    Created Admin endpoint

---
+++ src/main/java/com/todo/studentcoursesystem/Controller/AdminController.java
@@ -0,0 +1,38 @@
+package com.todo.studentcoursesystem.Controller;
+
+import com.todo.studentcoursesystem.Entity.Student;
+import com.todo.studentcoursesystem.Service.CourseService;
+import com.todo.studentcoursesystem.Service.EnrollmentService;
+import com.todo.studentcoursesystem.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+
+    private CourseService courseService;
+    private EnrollmentService enrollmentService;
+    private StudentService studentService;
+
+
+    public AdminController(CourseService courseService, EnrollmentService enrollmentService, StudentService studentService) {
+        this.courseService = courseService;
+        this.enrollmentService = enrollmentService;
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student
+                                                 ,@RequestHeader(value = "X-Admin",required = false) String xAdmin){
+        if (!"true".equals(xAdmin)) {
+            throw new RuntimeException("Access denied: Admin only");
+        }
+        Student createStudent=studentService.createStudent(student);
+        return ResponseEntity.ok(createStudent);
+
+
+    }
+}
